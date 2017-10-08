6b89a4b6c1215e0d08429e0d14f5e6aeacf52787
nerdypepper <akshayoppiliappan@gmail.com>
2017-10-08 21:16:33 +0530

    Add polybar autohide

--- scripts/scripts/hlws.py
+++ scripts/scripts/hlws.py
@@ -8,11 +8,11 @@ i = 0
 while i < index:
     if output[i] == ":":
         #occupied
-        wstr = wstr + "  *"
+        wstr = wstr + "  ·"
         i = i + 1
     elif output[i] == ".":
         #unoccupied
-        wstr = wstr + "  ·"
+        wstr = wstr + "  -"
         i = i + 1
     elif output[i] == "#":
         #current

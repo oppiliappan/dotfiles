5fd753d49e9bc528347d1a052e034dcf028d55e8
nerdypepper <akshayoppiliappan@gmail.com>
2018-02-17 15:42:01 +0530

    Invert border functionality

--- 2bwm/2bwm/config.h
+++ 2bwm/2bwm/config.h
@@ -28,10 +28,10 @@ static const uint8_t offsets[] = {0,0,0,0};
  *2)fixedcol         3)unkilcol
  *4)fixedunkilcol    5)outerbordercol
  *6)emptycol         */
-static const char *colors[] = {"#121c21","#121c21","#718c97","#fa9a6a","#ceb5ce","#1e2e36","#181818"};
+static const char *colors[] = {"#304b59","#1e2e36","#718c97","#fa9a6a","#ceb5ce","#121c21","#181818"};
 
 /* if this is set to true the inner border and outer borders colors will be swapped */
-static const bool inverted_colors = true;
+static const bool inverted_colors = false;
 
 ///---Cursor---///
 /* default position of the cursor:

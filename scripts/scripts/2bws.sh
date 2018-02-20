059f56e34a4983aa6c1721704364db37b7ff033c
nerdypepper <akshayoppiliappan@gmail.com>
2018-02-20 18:43:47 +0530

    Move to shell scripts

---
+++ scripts/scripts/2bws.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ws=$( xprop -root _NET_CURRENT_DESKTOP | sed -e 's/_NET_CURRENT_DESKTOP(CARDINAL) = //' )
+
+ws=$(( $ws + 1 ))
+
+draw(){
+	for i in {1..5}; do
+		if [[ $i -eq $ws ]]
+		then
+			echo -ne "%{F#f0696f} ∙"
+		else
+			echo -ne "%{F#aaafb2} ·"
+		fi
+	done
+}
+
+draw

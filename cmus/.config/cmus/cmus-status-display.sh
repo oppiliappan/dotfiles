4fcf72a01a804cb1daaa93b965f90cce7417888c
NerdyPepper <akshayoppiliappan@gmail.com>
2018-04-05 18:26:13 +0530

    Add urgency levels

--- cmus/.config/cmus/cmus-status-display.sh
+++ cmus/.config/cmus/cmus-status-display.sh
@@ -4,4 +4,4 @@ artist=$( cmus-remote -Q | grep tag\ artist\ | sed 's/tag artist //' )
 title=$( cmus-remote -Q | grep tag\ title\ | sed 's/tag title //' )
 album=$( cmus-remote -Q | grep tag\ album\ | sed 's/tag album //' )
 
-notify-send "${artist}" "${title} - ${album}"
+notify-send -u low "${artist}" "${title} - ${album}"

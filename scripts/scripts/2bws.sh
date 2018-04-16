a56a84a10e3f7601c6a3bd13507ac3821f714f41
NerdyPepper <akshayoppiliappan@gmail.com>
2018-04-16 16:29:33 +0530

    New icons, use vars

--- scripts/scripts/2bws.sh
+++ scripts/scripts/2bws.sh
@@ -4,6 +4,7 @@
 ws=$( xprop -root _NET_CURRENT_DESKTOP | sed -e 's/_NET_CURRENT_DESKTOP(CARDINAL) = //' )
 
 # icons
+
 CURRENT=▪
 OCCUPIED=▪
 UNOCCUPIED=▫
@@ -14,7 +15,7 @@ normal="#5c5c5c"
 
 # function to print workspaces to stdout
 draw() {
-	for i in {0..4}; do
+	for i in {0..5}; do
 		# get the number of windows in each workspace
 		windows=$( wmctrl -l | cut -d ' ' -f3 | grep $i | wc -l )
 

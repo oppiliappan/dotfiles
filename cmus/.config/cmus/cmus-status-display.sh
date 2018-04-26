5be30d6dca66e9d37c57af3746019b2a530f2911
NerdyPepper <akshayoppiliappan@gmail.com>
2018-04-26 12:15:05 +0530

    Improve notifs

--- cmus/.config/cmus/cmus-status-display.sh
+++ cmus/.config/cmus/cmus-status-display.sh
@@ -4,4 +4,5 @@ artist=$( cmus-remote -Q | grep tag\ artist\ | sed 's/tag artist //' )
 title=$( cmus-remote -Q | grep tag\ title\ | sed 's/tag title //' )
 album=$( cmus-remote -Q | grep tag\ album\ | sed 's/tag album //' )
 
-notify-send -u low "${title}" "${album} - ${artist}"
+xdotool key "ctrl+space" &> /dev/null
+notify-send -u low "${title}" "${album} · ${artist}"

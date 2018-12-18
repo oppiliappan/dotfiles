27eabbc8415df9e1edceec6c452f76b74b958661
NerdyPepper <akshayoppiliappan@gmail.com>
2018-12-18 15:05:03 +0530

    m a j o r

--- scripts/scripts/playpause.sh
+++ scripts/scripts/playpause.sh
@@ -6,11 +6,11 @@ if pgrep -x "cmus" > /dev/null
 then
 	status=$( cmus-remote -Q | grep status\ | sed 's/status //' )
 	if [[ "$status" == *playing*  ]]; then
-		echo -ne " %{F$(get_xres color2)} "
+		echo -ne " %{F$(get_xres color4)} "
 	elif [[ "$status" == *paused* ]]; then
-		echo -ne " %{F$(get_xres color2)} "
+		echo -ne " %{F$(get_xres color4)} "
 	elif [[ "$status" == *stopped* ]]; then
-		echo -ne " %{F$(get_xres color2)} "
+		echo -ne " %{F$(get_xres color4)} "
 	fi
 else
 	echo -ne "  "
